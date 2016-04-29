fdbe43893a56b6040575af353e7d58f929c30e4d
Björn Gustafsson <bjorn.gustafsson@tretton37.com>
2016-04-29 18:47:09 +0200

    Handle RangeError

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var hieroglyphy = require('hieroglyphy'),
 
 module.exports = function() {
 	return through2.obj(function(chunk, enc, cb) {
-		this.push(hieroglyphy.hieroglyphyScript('' + chunk));
+		try {
+			this.push(hieroglyphy.hieroglyphyString(chunk.contents.toString(enc)));
+		} catch (e) {
+			if (e instanceof RangeError) {
+				process.stderr.write("RangeError in gulp-heiroglyphy, most likely out of memory\n");
+			}
+			this.push(chunk);
+		}
 
 		cb();
 	});
